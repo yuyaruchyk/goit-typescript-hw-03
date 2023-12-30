d32f45f8f9c042225aaf9e083e314f863cba4cdd
yuyaruchyk <130397548+yuyaruchyk@users.noreply.github.com>
2023-12-30 21:49:21 +0100

    upd

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,44 @@
+class Key {
+  private signature: string;
+  constructor() {
+    this.signature = `${Math.random()}`;
+  }
+
+  getSignature() {
+    return this.signature;
+  }
+}
+class Person {
+  private key: Key;
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+class House {
+  door: boolean = false;
+  key: Key;
+  tenants: Person[] = [];
+  constructor(key: Key) {}
+
+  comeIn(newPerson: Person) {
+    if (this.door) {
+      this.tenants.push(newPerson);
+    }
+  }
+  openDoor(key: Key) {}
+}
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +47,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
